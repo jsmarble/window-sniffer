31c0950e65d9b25d48f12724af6b82c30efca2e2
jsmarble <jsmarble@3b9564d9-eb4e-0410-9f67-c9b49407e441>
2008-06-05 14:15:20 +0000

    Added File>Exit

--- WindowSniffer/WindowSnifferForm.cs
+++ WindowSniffer/WindowSnifferForm.cs
@@ -84,5 +84,10 @@ namespace WindowSniffer
         {
             RefreshWindows();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
